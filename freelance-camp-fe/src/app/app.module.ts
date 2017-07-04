0da64ab893c7451a62cd11a72de25276ffdcf643
Matt Warren <mwarren@harding.edu>
2017-07-04 15:18:06 -0400

    add bootstrap 4 to project

--- freelance-camp-fe/src/app/app.module.ts
+++ freelance-camp-fe/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -23,7 +24,8 @@ import { AppRoutingModule} from './app-routing.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
