9ef7eb2597f55caaab9a76806ce3d62c2caa3b96
Matt Warren <mwarren@harding.edu>
2017-07-04 14:04:33 -0400

    add new form to insert a new proposal

--- freelance-camp-fe/src/app/app.module.ts
+++ freelance-camp-fe/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -19,7 +20,8 @@ import { AppRoutingModule} from './app-routing.module'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
