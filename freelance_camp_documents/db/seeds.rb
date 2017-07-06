275fa1d593ec7a9dda574202fb93b9b49909d4d4
Matt Warren <mwarren@harding.edu>
2017-07-06 17:57:07 -0400

    add rack-cors gem for making ajax possible with angular. add seeds.

--- freelance_camp_documents/db/seeds.rb
+++ freelance_camp_documents/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |d|
+	FreelanceDocument.create!(
+		title: "Document #{d}", 
+		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore numquam a eius aut incidunt impedit, dolor temporibus corporis, sapiente, necessitatibus, maiores!", 
+		file_url: "http://www.pragtob.info/rails-beginner-cheatsheet/RailsBeginnerCheatSheetSinglePage.pdf",
+		image_url: "https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg"
+	)
+end
+
