afed8217ba854df5a16dd6f8aa1b1cca44bb549f
Matt Warren <mwarren@harding.edu>
2017-07-09 14:07:52 -0400

    relocate rails proposal and directory app directories to their own repositories

--- freelance_camp_proposal/app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: 'mwarren@harding.edu'
-  layout 'mailer'
-end
--- freelance_camp_proposal/app/mailers/proposal_mailer.rb
+++
@@ -1,6 +0,0 @@
-class ProposalMailer < ApplicationMailer
-	def email proposal
-		@proposal = proposal
-		mail(to: @proposal.client_email, subject: "You've received a new proposal")
-	end
-end
